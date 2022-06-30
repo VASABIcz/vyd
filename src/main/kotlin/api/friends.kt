9422c29d77eff828ce79eafe2e83565d3167671b
vasabi <vasabisroa@gmail.com>
2022-06-30 17:46:45 +0200

    messages, send endpoint

--- src/main/kotlin/api/friends.kt
+++ src/main/kotlin/api/friends.kt
@@ -5,12 +5,15 @@ import Parameters
 import Responses.Companion.badRequest
 import Responses.Companion.serverIssue
 import Responses.Companion.success
+import data.requests.MessagePayload
 import database.FriendService
 import database.UserService
 import database.fetchUser
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -75,6 +78,44 @@ fun Application.friends(userService: UserService, friendService: FriendService) 
                         } else {
                             return@put call.serverIssue()
                         }
+                    }
+                    post("send") {
+                        val payload = call.receive<MessagePayload>()
+                        val p = Parameters(call.parameters)
+                        val friendId by p.parameter("friend", Converter.Int)
+                        if (!p.isValid) {
+                            return@post call.badRequest(p.getIssues)
+                        }
+
+                        val principal = call.principal<JWTPrincipal>()
+                        val me = principal!!.fetchUser(userService)!!
+
+                        val friend = userService.getUser(friendId!!)
+
+                        if (friendService.sendMessage(me, friend!!, payload.content)) {
+                            return@post call.success()
+                        } else {
+                            return@post call.serverIssue()
+                        }
+                    }
+                    get("messages") {
+                        val p = Parameters(call.parameters)
+                        val friendId by p.parameter("friend", Converter.Int)
+                        if (!p.isValid) {
+                            return@get call.badRequest(p.getIssues)
+                        }
+
+                        val principal = call.principal<JWTPrincipal>()
+                        val me = principal!!.fetchUser(userService)!!
+
+                        val friend = userService.getUser(friendId!!)
+
+                        val messages =
+                            friendService.getMessages(me, friend!!) ?: return@get call.respond(HttpStatusCode.NotFound)
+
+                        return@get call.respond(messages.map {
+                            it.toMessagesMessage()
+                        })
                     }
                 }
             }
