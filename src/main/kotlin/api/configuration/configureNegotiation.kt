8c6772262cff010ba46ba4541ba069c5591cc3cc
vasabi <vasabisroa@gmail.com>
2022-06-30 16:01:24 +0200

    content negotiation configuration

---
+++ src/main/kotlin/api/configuration/configureNegotiation.kt
@@ -0,0 +1,17 @@
+package api.configuration
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.doublereceive.*
+import kotlinx.serialization.json.Json
+
+
+fun Application.configureNegotiation() {
+    install(DoubleReceive)
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+        })
+    }
+}
