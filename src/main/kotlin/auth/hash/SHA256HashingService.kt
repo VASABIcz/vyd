a1e24c29c8603ba0d6efa384a1eefda091c0ff3e
vasabi <vasabisroa@gmail.com>
2022-06-30 16:05:52 +0200

    fixed SHA256HashingService unable to verify

--- src/main/kotlin/auth/hash/SHA256HashingService.kt
+++ src/main/kotlin/auth/hash/SHA256HashingService.kt
@@ -8,12 +8,12 @@ class SHA256HashingService: HashingService {
     override fun generateSaltedHash(value: String, saltLenght: Int): SaltedHash {
         val salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLenght)
         val saltAsHex = Hex.encodeHexString(salt)
-        val hash = DigestUtils.sha256Hex("$salt$value")
+        val hash = DigestUtils.sha256Hex("$saltAsHex$value")
 
         return SaltedHash(hash, saltAsHex)
     }
 
     override fun verify(value: String, saltedHash: SaltedHash): Boolean {
-        return DigestUtils.sha256Hex("${saltedHash.salt}$value") == saltedHash.hash
+        return DigestUtils.sha256Hex("${saltedHash.salt}${value}") == saltedHash.hash
     }
 }
