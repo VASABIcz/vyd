542532d0e0deec07e40f690f4724278865fb5f25
vasabi <vasabisroa@gmail.com>
2022-07-01 08:46:25 +0200

    cors configuration

---
+++ src/main/kotlin/api/configuration/configureCors.kt
@@ -0,0 +1,11 @@
+package api.configuration
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+
+fun Application.configureCors() {
+    install(CORS) {
+        anyHost()
+        allowHeadersPrefixed("")
+    }
+}
