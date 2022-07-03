ea6827ebc2799b2b6ddc82a771c6c3166c6d948d
vasabi <vasabisroa@gmail.com>
2022-07-03 09:48:29 +0200

    allowing all cors

--- src/main/kotlin/api/configuration/configureCors.kt
+++ src/main/kotlin/api/configuration/configureCors.kt
@@ -1,11 +1,16 @@
 package api.configuration
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureCors() {
     install(CORS) {
+        // jesus i hate cors
+        allowNonSimpleContentTypes = true
+        allowCredentials = true
         anyHost()
         allowHeadersPrefixed("")
+        allowMethod(HttpMethod.Post)
     }
 }
