045e93608c0b27017cce66d53959e2782f897a8f
vasabi <vasabisroa@gmail.com>
2022-07-03 11:10:22 +0200

    FriendRequestService definition

--- src/main/kotlin/database/FriendRequestService.kt
+++ src/main/kotlin/database/FriendRequestService.kt
@@ -7,9 +7,9 @@ interface FriendRequestService {
 
     fun getRequestState(id: Int): DatabaseFriendRequest?
 
-    fun deletePendingRequest(requester: DatabaseUser, receiver: DatabaseUser): Boolean
+    // fun deletePendingRequest(requester: DatabaseUser, receiver: DatabaseUser): Boolean
 
-    fun changePendingRequestState(requester: DatabaseUser, receiver: DatabaseUser, state: FriendRequestState): Boolean
+    // fun changePendingRequestState(requester: DatabaseUser, receiver: DatabaseUser, state: FriendRequestState): Boolean
 
     fun changePendingRequestState(id: Int, state: FriendRequestState, receiver: DatabaseUser): Boolean
 
