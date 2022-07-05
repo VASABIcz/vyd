64568d9afe61dfd15fb98456c308e4cf6e680887
vasabi <vasabisroa@gmail.com>
2022-07-05 12:39:50 +0200

    rename

--- src/main/kotlin/data/responses/FriendsFriend.kt
+++ src/main/kotlin/data/responses/FriendsFriend.kt
@@ -1,4 +1,4 @@
 package data.responses
 
 @kotlinx.serialization.Serializable
-data class FriendsFriend(val id: UsersUser, val channel: Int, val timestamp: Long, val friends: Boolean)
+data class FriendsFriend(val friend: UsersUser, val channel: Int, val timestamp: Long, val friends: Boolean)
