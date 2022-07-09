297207061239b7f042fa05560d8b87be228663f3
vasabi <vasabisroa@gmial.com>
2022-07-10 01:23:43 +0200

    code rearrangement

--- src/main/kotlin/api/users.kt
+++ src/main/kotlin/api/users.kt
@@ -4,8 +4,9 @@ import Converter
 import Parameters
 import Responses.Companion.badRequest
 import Responses.Companion.notFound
-import database.fetchUser
+import Responses.Companion.serverIssue
 import database.servicies.users.UserService
+import database.servicies.users.userId
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -13,35 +14,48 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.users(
-    userService: UserService,
+    userService: UserService
 ) {
     routing {
-        route("/users/") {
-            get("/name/{name}/{descriptor}") {
-                val p = Parameters(call.parameters)
-                val name by p.parameter("name", Converter.String)
-                val descriptor by p.parameter("descriptor", Converter.String)
-                if (!p.isValid) {
-                    return@get call.badRequest(p.getIssues)
-                }
-                val u = userService.getUser(name!!, descriptor!!) ?: return@get call.notFound()
-                call.respond(u.toUsersUser())
-            }
-            get("/id/{id}") {
-                val p = Parameters(call.parameters)
-                val id by p.parameter("id", Converter.Int)
-                if (!p.isValid) {
-                    return@get call.badRequest(p.getIssues)
-                }
-                val u = userService.getUser(id!!) ?: return@get call.notFound()
-                call.respond(u.toUsersUser())
-            }
+        route("users") {
             authenticate {
-                get("/@me") {
-                    val principal = call.principal<JWTPrincipal>()
-                    val me = principal!!.fetchUser(userService)!!
-
-                    call.respond(me.toUsersUser())
+                route("id") {
+                    route("{user_id}") {
+                        get {
+                            val p = Parameters(call.parameters)
+                            val id by p.parameter("id", Converter.Int)
+                            if (!p.isValid) {
+                                return@get call.badRequest(p.getIssues)
+                            }
+                            val u = userService.getUser(id!!) ?: return@get call.notFound()
+                            call.respond(u.toUsersUser())
+                        }
+                    }
+                }
+                route("username") {
+                    route("{username}") {
+                        route("{discriminator}") {
+                            get {
+                                val p = Parameters(call.parameters)
+                                val name by p.parameter("username", Converter.String)
+                                val descriptor by p.parameter("discriminator", Converter.String)
+                                if (!p.isValid) {
+                                    return@get call.badRequest(p.getIssues)
+                                }
+                                val u = userService.getUser(name!!, descriptor!!) ?: return@get call.notFound()
+                                call.respond(u.toUsersUser())
+                            }
+                        }
+                    }
+                }
+                authenticate {
+                    route("@me") {
+                        get {
+                            val me = call.principal<JWTPrincipal>()?.userId!!
+                            val user = userService.getUser(me) ?: return@get call.serverIssue()
+                            call.respond(user.toUsersUser())
+                        }
+                    }
                 }
             }
         }
