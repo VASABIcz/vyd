32fcb22a4e3a28b83ae1511b73fb0fbca06f5bdc
vasabi <vasabisroa@gmial.com>
2022-07-10 01:25:55 +0200

    added dependencies

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
-import api.auth
+import api.*
 import api.configuration.configureCallLogging
 import api.configuration.configureCors
 import api.configuration.configureMetrics
 import api.configuration.configureNegotiation
-import api.configureSecurity
-import api.friends
-import api.users
 import auth.hash.SHA256HashingService
 import auth.token.JwtService
 import auth.token.TokenConfig
+import database.servicies.channels.DatabaseChannelService
 import database.servicies.friendRequests.DatabaseFriendRequestService
 import database.servicies.friends.DatabaseFriendService
+import database.servicies.guilds.DatabaseGuildChannelService
+import database.servicies.guilds.DatabaseGuildMemberService
+import database.servicies.guilds.DatabaseGuildService
+import database.servicies.messages.DatabaseMessageService
 import database.servicies.usernames.DatabaseUsernameService
 import database.servicies.users.DatabaseUserService
 import io.ktor.server.application.*
@@ -19,6 +21,7 @@ import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.database.Database
+import wrapers.*
 
 
 fun main() {
@@ -33,18 +36,39 @@ fun main() {
             user = System.getenv("database_username"),
             password = System.getenv("database_password")
         )
+
         val usernameService = DatabaseUsernameService(database)
-        val userService = DatabaseUserService(database, usernameService)
+        val userService = DatabaseUserService(database)
         val hashingService = SHA256HashingService()
         val tokenService = JwtService()
         val friendService = DatabaseFriendService(database)
-        val friendRequestService = DatabaseFriendRequestService(database, friendService)
+        val friendRequestService = DatabaseFriendRequestService(database)
+        val guildService = DatabaseGuildService(database)
+        val guildMemberService = DatabaseGuildMemberService(database)
+        val guildChannelService = DatabaseGuildChannelService(database)
+        val channelService = DatabaseChannelService(database)
+        val messageService = DatabaseMessageService(database)
+
         val config = TokenConfig(
             issuer = "http://${System.getenv("host")}:${System.getenv("port").toInt()}",
             audience = "users",
             expiresIn = 1000L * 60L * 60L * 24L * 365L,
             secret = System.getenv("secret")
         )
+
+        val friendRequestWrapper = FriendRequestWrapper(database, friendRequestService, friendService)
+        val friendWrapper = FriendWrapper(friendService, messageService)
+        val userWrapper = UserWrapper(database, userService, usernameService, hashingService)
+        val hashWrapper = HashWrapper(hashingService, userService, tokenService, config)
+        val guildWrapper = GuildWrapper(
+            database,
+            guildService,
+            guildMemberService,
+            guildChannelService,
+            channelService,
+            messageService
+        )
+
         routing {
             get("/") {
                 call.respond("Hello world")
@@ -56,8 +80,10 @@ fun main() {
         configureCallLogging()
         configureMetrics()
 
-        auth(userService, hashingService, tokenService, config)
-        friends(userService, friendService, friendRequestService)
+        auth(userService, hashingService, tokenService, config, userWrapper, hashWrapper)
+        friends(friendService, friendWrapper)
         users(userService)
+        guilds(guildMemberService, guildWrapper)
+        friendRequests(friendRequestService, friendRequestWrapper)
     }.start(wait = true)
 }
