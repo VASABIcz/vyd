b607ce66e1f6522bfdc0ac4ac19d79e39cbafea3
vasabi <vasabisroa@gmial.com>
2022-07-10 01:25:39 +0200

    friendrequests api endpoint

--- src/main/kotlin/api/friendRequests.kt
+++ src/main/kotlin/api/friendRequests.kt
@@ -1,61 +1,105 @@
 package api
 
+import Converter
+import Parameters
+import Responses.Companion.badRequest
 import Responses.Companion.serverIssue
 import Responses.Companion.success
-import data.requests.CreateFriendRequest
-import data.requests.RespondFriendRequest
-import database.servicies.friendRequests.FriendRequestResponse
 import database.servicies.friendRequests.FriendRequestService
-import database.servicies.users.UserService
-import database.servicies.users.fetchUser
+import database.servicies.users.userId
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import wrapers.FriendRequestWrapper
 
+
 fun Application.friendRequests(
     friendRequestService: FriendRequestService,
-    userService: UserService,
     friendRequestWrapper: FriendRequestWrapper
 ) {
     routing {
-        authenticate {
-            route("friendrequests") {
-                put { // accept/decline request
-                    val response = call.receive<RespondFriendRequest>()
-
-                    val principal = call.principal<JWTPrincipal>()
-                    val me = principal!!.fetchUser(userService)!!
-
-                    if (response.response == FriendRequestResponse.accept) {
-                        friendRequestWrapper.acceptFriendRequest(response.id, me.id)
-                    } else {
-                        friendRequestWrapper.declineFriend(response.id, me.id)
-                    }
-                }
-                post { // create request
-                    val response = call.receive<CreateFriendRequest>() // TODO accept username
-                    val principal = call.principal<JWTPrincipal>()
-                    val me = principal!!.fetchUser(userService)!!
-
-                    if (friendRequestService.createRequest(me.id, response.receiver)) {
-                        return@post call.success()
-                    } else {
-                        return@post call.serverIssue()
-                    }
-                }
+        route("friendrequests") {
+            authenticate {
                 get {
-                    val principal = call.principal<JWTPrincipal>()
-                    val me = principal!!.fetchUser(userService)!!
+                    val me = call.principal<JWTPrincipal>()!!.userId!!
 
-                    call.respond(
-                        friendRequestService.getPendingRequests(me.id).map {
-                            it.toFriendRequestsRequest()
-                        }.toList()
-                    )
+                    val requests = friendRequestService.getPendingRequests(me).map {
+                        it.toFriendRequestsRequest()
+                    }
+
+                    call.respond(requests)
+                }
+
+                route("create") {
+                    // TODO
+                    post("{user_id}") {
+                        val p = Parameters(call.parameters)
+                        val userId by p.parameter("user_id", Converter.Int)
+                        if (!p.isValid) {
+                            return@post call.badRequest(p.getIssues)
+                        }
+
+                        val me = call.principal<JWTPrincipal>()!!.userId!!
+
+                        if (friendRequestService.createRequest(me, userId!!)) {
+                            call.success()
+                        } else {
+                            call.serverIssue()
+                        }
+                    }
+                }
+
+                route("request") {
+                    route("{request_id}") {
+                        patch("accept") {
+                            val p = Parameters(call.parameters)
+                            val requestId by p.parameter("request_id", Converter.Int)
+                            if (!p.isValid) {
+                                return@patch call.badRequest(p.getIssues)
+                            }
+
+                            val me = call.principal<JWTPrincipal>()!!.userId!!
+
+                            if (friendRequestWrapper.acceptFriendRequest(requestId!!, me)) {
+                                call.success()
+                            } else {
+                                call.serverIssue()
+                            }
+                        }
+                        patch("reject") {
+                            val p = Parameters(call.parameters)
+                            val requestId by p.parameter("request_id", Converter.Int)
+                            if (!p.isValid) {
+                                return@patch call.badRequest(p.getIssues)
+                            }
+
+                            val me = call.principal<JWTPrincipal>()!!.userId!!
+
+                            if (friendRequestWrapper.declineFriend(requestId!!, me)) {
+                                call.success()
+                            } else {
+                                call.serverIssue()
+                            }
+                        }
+                        /*
+                        get {
+                            val p = Parameters(call.parameters)
+                            val requestId by p.parameter("request_id", Converter.Int)
+                            if (!p.isValid) {
+                                return@get call.badRequest(p.getIssues)
+                            }
+
+                            val me = call.principal<JWTPrincipal>()!!.userId!!
+
+                            val request = friendRequestService.getRequestState(requestId!!)?.toFriendRequestsRequest() ?: return@get call.serverIssue()
+
+                            call.respond(request)
+                        }
+
+                         */
+                    }
                 }
             }
         }
