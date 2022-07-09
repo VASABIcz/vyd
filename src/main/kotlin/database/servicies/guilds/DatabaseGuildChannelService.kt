da477868f3144aca811e2b58c9565cc6f1accdf1
vasabi <vasabisroa@gmial.com>
2022-07-10 01:18:42 +0200

    accepts basic data types

--- src/main/kotlin/database/servicies/guilds/DatabaseGuildChannelService.kt
+++ src/main/kotlin/database/servicies/guilds/DatabaseGuildChannelService.kt
@@ -1,6 +1,7 @@
 package database.servicies.guilds
 
 import org.ktorm.database.Database
+import org.ktorm.dsl.and
 import org.ktorm.dsl.eq
 import org.ktorm.entity.filter
 import org.ktorm.entity.find
@@ -10,12 +11,12 @@ import org.ktorm.entity.toList
 class DatabaseGuildChannelService(private val database: Database) : GuildChannelService {
     val channels get() = database.sequenceOf(DatabaseGuildChannels)
 
-    override fun moveChannel(channel: Int, position: Int): Boolean {
+    override fun moveChannel(channel: Int, guild: Int, position: Int): Boolean {
         TODO("Not yet implemented")
     }
 
-    override fun editChannel(id: Int, name: String): Boolean {
-        val channel = getChannel(id) ?: return false
+    override fun editChannel(id: Int, guild: Int, name: String): Boolean {
+        val channel = getChannel(id, guild) ?: return false
         channel.name = name
         return channel.flushChanges() > 0
     }
@@ -26,9 +27,9 @@ class DatabaseGuildChannelService(private val database: Database) : GuildChannel
         }.toList()
     }
 
-    override fun getChannel(id: Int): DatabaseGuildChannel? {
+    override fun getChannel(id: Int, guild: Int): DatabaseGuildChannel? {
         return channels.find {
-            DatabaseGuildChannels.channel_id eq id
+            (it.channel_id eq id) and (it.guild_id eq guild)
         }
     }
 }
