73f975a7782a36c601571c710efa0dfa4e10e683
vasabi <vasabisroa@gmial.com>
2022-07-10 01:18:00 +0200

    orm scheme

--- src/main/kotlin/database/servicies/channels/channelsScheme.kt
+++ src/main/kotlin/database/servicies/channels/channelsScheme.kt
@@ -1,5 +1,6 @@
 package database.servicies.channels
 
+import data.responses.ChannelsChannel
 import org.ktorm.entity.Entity
 import org.ktorm.schema.Table
 import org.ktorm.schema.enum
@@ -7,6 +8,7 @@ import org.ktorm.schema.int
 import org.ktorm.schema.timestamp
 import java.time.Instant
 
+@kotlinx.serialization.Serializable
 enum class ChannelType {
     friends, text, voice, group, category
 }
@@ -17,6 +19,10 @@ interface DatabaseChannel : Entity<DatabaseChannel> {
     val id: Int
     val timestamp: Instant
     val type: ChannelType
+
+    fun toChannelsChannel(): ChannelsChannel {
+        return ChannelsChannel(id, timestamp.toEpochMilli(), type)
+    }
 }
 
 object DatabaseChannels : Table<DatabaseChannel>("channels") {
