7747822f250814a2f22bfb464e92cb74c259ad4e
vasabi <vasabisroa@gmial.com>
2022-07-10 01:20:34 +0200

    accepts basic data types

--- src/main/kotlin/database/servicies/guilds/GuildChannelService.kt
+++ src/main/kotlin/database/servicies/guilds/GuildChannelService.kt
@@ -6,12 +6,12 @@ interface GuildChannelService {
     // fun deleteChannel(guild: Int, channel: Int)
     // FIXME not sure if make custom implementation or depend on ChannelService + foregin key casacade
 
-    fun moveChannel(channel: Int, position: Int): Boolean
+    fun moveChannel(channel: Int, guild: Int, position: Int): Boolean
     // TODO implementation with categories
 
-    fun editChannel(id: Int, name: String): Boolean
+    fun editChannel(id: Int, guild: Int, name: String): Boolean
 
     fun getChannels(guild: Int): List<DatabaseGuildChannel>
 
-    fun getChannel(id: Int): DatabaseGuildChannel?
+    fun getChannel(id: Int, guild: Int): DatabaseGuildChannel?
 }
