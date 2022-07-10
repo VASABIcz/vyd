ad8fac150fabc3b4fd62fd02c382096727917b62
vasabi <vasabisroa@gmial.com>
2022-07-10 19:58:28 +0200

    rearrangement

--- src/main/kotlin/api/users.kt
+++ src/main/kotlin/api/users.kt
@@ -14,43 +14,41 @@ fun Application.users(
 ) {
     routing {
         route("users") {
-            authenticate {
-                route("id") {
-                    route("{user_id}") {
+            route("id") {
+                route("{user_id}") {
+                    get {
+                        val p = Parameters(call.parameters)
+                        val id by p.parameter("user_id", Converter.Int)
+                        if (!p.isValid) {
+                            return@get call.badRequest(p.getIssues)
+                        }
+                        val u = userService.getUser(id!!) ?: return@get call.notFound()
+                        call.respond(u.toUsersUser())
+                    }
+                }
+            }
+            route("username") {
+                route("{username}") {
+                    route("{discriminator}") {
                         get {
                             val p = Parameters(call.parameters)
-                            val id by p.parameter("id", Converter.Int)
+                            val name by p.parameter("username", Converter.String)
+                            val descriptor by p.parameter("discriminator", Converter.String)
                             if (!p.isValid) {
                                 return@get call.badRequest(p.getIssues)
                             }
-                            val u = userService.getUser(id!!) ?: return@get call.notFound()
+                            val u = userService.getUser(name!!, descriptor!!) ?: return@get call.notFound()
                             call.respond(u.toUsersUser())
                         }
                     }
                 }
-                route("username") {
-                    route("{username}") {
-                        route("{discriminator}") {
-                            get {
-                                val p = Parameters(call.parameters)
-                                val name by p.parameter("username", Converter.String)
-                                val descriptor by p.parameter("discriminator", Converter.String)
-                                if (!p.isValid) {
-                                    return@get call.badRequest(p.getIssues)
-                                }
-                                val u = userService.getUser(name!!, descriptor!!) ?: return@get call.notFound()
-                                call.respond(u.toUsersUser())
-                            }
-                        }
-                    }
-                }
-                authenticate {
-                    route("@me") {
-                        get {
-                            val me = call.principal<JWTPrincipal>()?.userId!!
-                            val user = userService.getUser(me) ?: return@get call.serverIssue()
-                            call.respond(user.toUsersUser())
-                        }
+            }
+            authenticate {
+                route("@me") {
+                    get {
+                        val me = call.principal<JWTPrincipal>()?.userId!!
+                        val user = userService.getUser(me) ?: return@get call.serverIssue()
+                        call.respond(user.toUsersUser())
                     }
                 }
             }
