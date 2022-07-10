2c1e0942de458fe0293018558bbb908aade793ae
vasabi <vasabisroa@gmial.com>
2022-07-10 19:59:47 +0200

    added password/username criteria

--- src/main/kotlin/wrapers/UserWrapper.kt
+++ src/main/kotlin/wrapers/UserWrapper.kt
@@ -11,13 +11,27 @@ class UserWrapper(
     private val usernameService: UsernameService,
     private val hashingService: HashingService
 ) {
+    private fun isValidPassword(password: String): Boolean {
+        // FIXME
+        return !(password.length < 4 || password.isBlank())
+    }
+
+    private fun isValidUsername(username: String): Boolean {
+        // FIXME
+        return !(username.isBlank() || username.contains(" "))
+    }
+
     fun createUser(username: String, password: String): Int? {
+        if (!isValidPassword(password) || !isValidUsername(username)) {
+            return null
+        }
+
         val discriminator = usernameService.getDiscriminator(username) ?: return null
         val hash = hashingService.generateSaltedHash(password)
         return database.useTransaction {
             val id = userService.createUser(username, hash, discriminator) ?: throw Throwable("failed to create user")
             if (!usernameService.incrementDiscriminator(username)) {
-                throw Throwable("failed to icnrement username")
+                throw Throwable("failed to increment username")
             }
             id
         }
