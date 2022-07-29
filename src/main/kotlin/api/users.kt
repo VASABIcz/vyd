2c090636a886044e6cfdd7667eb8cf54f5704ec9
vasabi <vasabisroa@gmial.com>
2022-07-29 21:04:47 +0200

    avatars

--- src/main/kotlin/api/users.kt
+++ src/main/kotlin/api/users.kt
@@ -1,16 +1,25 @@
 import Responses.Companion.badRequest
 import Responses.Companion.notFound
 import Responses.Companion.serverIssue
+import Responses.Companion.success
+import database.servicies.avatars.AvatarOwner
 import database.servicies.users.UserService
 import database.servicies.users.userId
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import wrapers.AvatarWrapper
+import java.io.InputStream
 
 fun Application.users(
-    userService: UserService
+    userService: UserService,
+    avatarWrapper: AvatarWrapper
 ) {
     routing {
         route("users") {
@@ -24,6 +33,19 @@ fun Application.users(
                         }
                         val u = userService.getUser(id!!) ?: return@get call.notFound()
                         call.respond(u.toUsersUser())
+                    }
+                    route("avatar") {
+                        get {
+                            val p = Parameters(call.parameters)
+                            val id by p.parameter("user_id", Converter.Int)
+                            if (!p.isValid) {
+                                return@get call.badRequest(p.getIssues)
+                            }
+
+                            val avatar = avatarWrapper.getUserAvatar(id!!)
+
+                            call.respondBytes(avatar, ContentType.Image.PNG, HttpStatusCode.OK)
+                        }
                     }
                 }
             }
@@ -43,12 +65,34 @@ fun Application.users(
                     }
                 }
             }
+            // TODO refactor
             authenticate {
                 route("@me") {
                     get {
                         val me = call.principal<JWTPrincipal>()?.userId!!
                         val user = userService.getUser(me) ?: return@get call.serverIssue()
                         call.respond(user.toUsersUser())
+                    }
+                    route("avatar") {
+                        patch {
+                            val me = call.principal<JWTPrincipal>()?.userId!!
+                            val user = userService.getUser(me) ?: return@patch call.serverIssue()
+                            call.respond(user.toUsersUser())
+
+
+                            var image: ByteArray
+                            withContext(Dispatchers.IO) {
+                                call.receive<InputStream>().use {
+                                    image = it.readBytes()
+                                }
+                            }
+
+                            if (avatarWrapper.createAvatar(user.id, AvatarOwner.user, image)) {
+                                call.success()
+                            } else {
+                                call.serverIssue()
+                            }
+                        }
                     }
                 }
             }
