03bfc99248f280c9235e1594f4cb640a892474a2
vasabi <vasabisroa@gmial.com>
2022-07-29 21:02:51 +0200

    nick is nullable

--- src/main/kotlin/data/responses/MembersMember.kt
+++ src/main/kotlin/data/responses/MembersMember.kt
@@ -2,4 +2,4 @@ package data.responses
 
 
 @kotlinx.serialization.Serializable
-data class MembersMember(val user: UsersUser, val nick: String, val guild: GuildsGuild, val timestamp: Long)
+data class MembersMember(val user: UsersUser, val nick: String?, val guild: GuildsGuild, val timestamp: Long)
