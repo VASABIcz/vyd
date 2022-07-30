207d87b3a1d5cbea9f8b4bfd84fbfdf64d82c4a5
vasabi <vasabisroa@gmial.com>
2022-07-30 11:13:15 +0200

    removed move channel added create channel

--- src/main/kotlin/database/servicies/guilds/GuildChannelService.kt
+++ src/main/kotlin/database/servicies/guilds/GuildChannelService.kt
@@ -1,13 +1,10 @@
 package database.servicies.guilds
 
 interface GuildChannelService {
-    // fun createChannel(guild: Int, type: ChannelType, name: String): Int?
+    fun createChannel(guild: Int, channel: Int, name: String): Int?
 
     // fun deleteChannel(guild: Int, channel: Int)
     // FIXME not sure if make custom implementation or depend on ChannelService + foregin key casacade
-
-    fun moveChannel(channel: Int, guild: Int, position: Int): Boolean
-    // TODO implementation with categories
 
     fun editChannel(id: Int, guild: Int, name: String): Boolean
 
