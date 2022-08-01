18101b74d166b03c925924fd7298879ab1e59a0f
VASABIcz <vasabisroa@gmail.com>
2022-08-01 14:29:25 +0200

    coroutines

--- src/main/kotlin/database/servicies/users/DatabaseUserService.kt
+++ src/main/kotlin/database/servicies/users/DatabaseUserService.kt
@@ -1,6 +1,8 @@
 package database.servicies.users
 
 import io.ktor.server.auth.jwt.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.ktorm.database.Database
 import org.ktorm.dsl.and
 import org.ktorm.dsl.eq
@@ -12,39 +14,35 @@ import utils.hash.SaltedHash
 class DatabaseUserService(private val database: Database) : UserService {
     private val users get() = database.sequenceOf(DatabaseUsers)
 
-    override fun createUser(username: String, hash: SaltedHash, discriminator: String): Int? {
-        return database.insertAndGenerateKey(DatabaseUsers) {
-            set(DatabaseUsers.name, username)
-            set(DatabaseUsers.discriminator, discriminator)
-            set(DatabaseUsers.hash, hash.hash.toByteArray())
-            set(DatabaseUsers.salt, hash.salt.toByteArray())
-        } as Int?
-    }
+    override suspend fun createUser(username: String, hash: SaltedHash, discriminator: String): Int? =
+        withContext(Dispatchers.IO) {
+            return@withContext database.insertAndGenerateKey(DatabaseUsers) {
+                set(DatabaseUsers.name, username)
+                set(DatabaseUsers.discriminator, discriminator)
+                set(DatabaseUsers.hash, hash.hash.toByteArray())
+                set(DatabaseUsers.salt, hash.salt.toByteArray())
+            } as Int?
+        }
 
-    override fun getUser(username: String, discriminator: String): DatabaseUser? {
-        return users.find {
+    override suspend fun getUser(username: String, discriminator: String): DatabaseUser? = withContext(Dispatchers.IO) {
+        return@withContext users.find {
             (DatabaseUsers.name eq username) and (DatabaseUsers.discriminator eq discriminator)
         }
     }
 
-    override fun getUser(id: Int): DatabaseUser? {
-        return users.find {
+    override suspend fun getUser(id: Int): DatabaseUser? = withContext(Dispatchers.IO) {
+        return@withContext users.find {
             (DatabaseUsers.id eq id)
         }
     }
 
-    override fun deleteUser(id: Int): Boolean {
-        val user = getUser(id) ?: return false
-        return user.delete() != 0
-    }
-
-    override fun deleteUser(username: String, discriminator: String): Boolean {
-        val user = getUser(username, discriminator) ?: return false
-        return user.delete() != 0
+    override suspend fun deleteUser(id: Int): Boolean = withContext(Dispatchers.IO) {
+        val user = getUser(id) ?: return@withContext false
+        return@withContext user.delete() != 0
     }
 }
 
-fun JWTPrincipal.fetchUser(userService: UserService): User? {
+suspend fun JWTPrincipal.fetchUser(userService: UserService): User? {
     return this.getClaim("id", Int::class)?.let {
         userService.getUser(it)
     }
