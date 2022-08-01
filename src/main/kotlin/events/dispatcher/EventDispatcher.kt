09d9d2e4e7807f448ce124fa804e7c462a4fc876
VASABIcz <vasabisroa@gmail.com>
2022-08-01 14:27:53 +0200

    unused

--- src/main/kotlin/events/dispatcher/EventDispatcher.kt
+++
@@ -1,9 +0,0 @@
-package events.dispatcher
-
-interface EventDispatcher {
-    suspend fun dispatch(message: kotlinx.serialization.Serializable)
-
-    suspend fun dispatch(message: String)
-
-    suspend fun dispatch(message: ByteArray)
-}
