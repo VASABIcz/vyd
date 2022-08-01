d7f7098c0e02f21946561fe5efa10c0cd61a6b89
VASABIcz <vasabisroa@gmail.com>
2022-08-01 14:31:54 +0200

    websocket gateway

---
+++ src/main/kotlin/websockets/gateway/Gateway.kt
@@ -0,0 +1,33 @@
+package websockets.gateway
+
+import database.servicies.friends.FriendService
+import database.servicies.guilds.GuildMemberService
+import database.servicies.users.userId
+import io.github.crackthecodeabhi.kreds.connection.Endpoint
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import websockets.worker.RedisConnection
+
+fun Application.gateway(
+    guildMemberService: GuildMemberService,
+    friendService: FriendService
+) {
+    routing {
+        authenticate {
+            webSocket("gateway") {
+                val me = call.principal<JWTPrincipal>()!!.userId!!
+                val con = RedisConnection(
+                    Endpoint.from(System.getenv("redis_host")),
+                    me,
+                    this,
+                    guildMemberService,
+                    friendService
+                )
+                con.connect().await()
+            }
+        }
+    }
+}
